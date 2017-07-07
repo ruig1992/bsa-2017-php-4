79a0cd3f66bb746ebaf992b54516bb155ce0b193
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-07-07 18:55:20 +0300

    Added infinite loop and processing user input

--- src/Game/Game.php
+++ src/Game/Game.php
@@ -11,17 +11,37 @@ class Game
 
     public function start(Reader $reader, Writer $writer): void
     {
+        $writer->writeln("You can't play yet. Please read input and convert it to commands.");
+        $writer->writeln("Don't forget to create game's world.");
+
+        $writer->write("Type your name: ");
+        $input = trim($reader->read());
+
+        $writer->writeln("Good luck with this game, {$input}!");
+        $writer->writeln("===================================");
+
         //TODO: Implement infinite loop and process user's input
 
         // Feel free to delete these lines
 
-        $writer->writeln("You can't play yet. Please read input and convert it to commands.");
-        $writer->writeln("Don't forget to create game's world.");
+        do {
+            $writer->write("Enter something: ");
+            $input = trim($reader->read());
 
-        $writer->write("Type your name:");
-        $input = trim($reader->read());
+            if ($input === 'error') {
+                //throw new ErrorException('Error 1');
+                //error_log('Error 1');
+                break;
+            }
 
-        $writer->writeln("Good luck with this task, {$input}!");
+            if ($input === 'exit') {
+                $writer->writeln("Game over! Bye!");
+                break;
+            }
+
+            $writer->writeln("--- You entered - {$input}\n");
+
+        } while (true);
     }
 
     public function run(Reader $reader, Writer $writer)
