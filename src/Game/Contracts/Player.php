6c9190e16071edaf473967b1d430112b42dacd45
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-07-07 19:55:21 +0300

    Added coins and room functionality into Player

--- src/Game/Contracts/Player.php
+++ src/Game/Contracts/Player.php
@@ -7,5 +7,10 @@ interface Player
     const DEFAULT_NAME = 'Unnamed Gamer';
 
     public function setName(string $name): self;
+    public function setCoin(): self;
+    public function setRoom($room): self;
+
     public function getName(): string;
+    public function getCoins(): int;
+    public function getRoom(): string;
 }
--- src/Game/DefaultPlayer.php
+++ src/Game/DefaultPlayer.php
@@ -11,6 +11,16 @@ class DefaultPlayer implements Player
      * @var string
      */
     protected $name;
+    /**
+     * Number of coins
+     * @var integer
+     */
+    protected $coins = 0;
+    /**
+     * Room instance, where the player is located
+     * @var Room;
+     */
+    protected $room;
 
     /**
      * Set player's name
@@ -23,7 +33,24 @@ class DefaultPlayer implements Player
         $this->name = $name ?: self::DEFAULT_NAME;
         return $this;
     }
-
+    /**
+     * Set one coin
+     * @return $this
+     */
+    public function setCoin(): Player
+    {
+        ++$this->coins;
+    }
+    /**
+     * Set player's room
+     * @param string $name
+     * @return $this
+     */
+    public function setRoom($room): Player
+    {
+        $this->room = $room;
+        return $this;
+    }
     /**
      * Get player's name
      * @return string
@@ -31,5 +58,21 @@ class DefaultPlayer implements Player
     public function getName(): string
     {
         return $this->name;
+    }
+    /**
+     * Get player's coins
+     * @return int
+     */
+    public function getCoins(): int
+    {
+        return $this->coins;
+    }
+    /**
+     * Get player's room
+     * @return string
+     */
+    public function getRoom(): string
+    {
+        return $this->room;
     }
 }
