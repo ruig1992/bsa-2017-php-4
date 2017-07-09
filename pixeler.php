94c9f64d5e64b1eadfcc135ca3ae0ed7a1a55151
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-07-09 15:01:41 +0300

    Added Pixeler library for image cli rendering, added and updated template files

---
+++ pixeler.php
@@ -0,0 +1,25 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$defaultImage = 'castle.jpg';
+
+// Parse options from command line
+$opts = array_merge([
+    'd' => 1,    // Dithering mode : 0 = DITHER_NONE, 1 = DITHER_ERROR
+    'f' => $defaultImage,
+    'r' => 1.0,  // Resize factor 1.0 = 100%
+    'w' => 0.95, // Dither treshold weight
+], getopt("f:r:w:d:ib"));
+
+// An image file/url is required.
+$opts['f'] || die("Must specify an image file.\n");
+
+// The -i option inverts the image
+$image = Pixeler\Pixeler::image($opts['f'], $opts['r'], isset($opts['i']), $opts['w'], $opts['d']);
+
+// No colors if "-b" is passed
+isset($opts['b']) && $image->clearColors();
+
+// The Pixeler\Image instance render itself if casted to a string
+echo $image;
---
+++ src/files/config.php
@@ -0,0 +1,5 @@
+<?php
+
+define('APP_ROOT', getenv('PWD') . '/src/');
+define('APP_IMAGES', APP_ROOT . 'files/images/');
+define('APP_TEMPLATES', APP_ROOT . 'files/templates/');
